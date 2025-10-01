db5c0f14deb0c1fd9a40a2fe6a2238f4e3507949
fwmakc <fwmakcpro@yandex.ru>
2025-10-01 16:49:45 +0300

    loop and wait to separate library

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,9 @@ import { hideCursor } from './helpers/hide_сursor.helper';
 import { initScreen } from './helpers/init_screen.helper';
 import { input } from './helpers/input.helper';
 import { inputch } from './helpers/inputch.helper';
-import { loop } from './helpers/loop.helper';
 import { position } from './helpers/position.helper';
 import { print } from './helpers/print.helper';
 import { showCursor } from './helpers/show_сursor.helper';
-import { wait } from './helpers/wait.helper';
 
 export {
   addch,
@@ -21,9 +19,7 @@ export {
   initScreen,
   input,
   inputch,
-  loop,
   position,
   print,
   showCursor,
-  wait,
 };
